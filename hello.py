40c57e5995827fe07013898ef066175c375b73f9
Axtris <91676948+Axtris@users.noreply.github.com>
2021-11-29 10:33:55 -0300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+List1 = [10,20,4,45,99]
+mx = max(List1[0], List1[1])
+secondmax = min(List1 [0], List1[1])
+n = len(List1)
+for i in range (2,n):
+    if listl[i]>mx:
+        Secondmax = mx
+        mx = listl[i]
+    elif listl[i]>secondmax and mx # listl [i] 
+    Secondmax = listl [i]
+
+print("second highest number is :", str (secondmax))
